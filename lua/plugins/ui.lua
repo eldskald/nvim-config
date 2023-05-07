16913dd0f6bc23d8c9fd5fa2f62f02d407bcfb8a
rafael <rafaelbordoni00@gmail.com>
2023-05-07 01:23:15 -0300

    Update plugins file structure

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,14 @@
+return {
+  "akinsho/bufferline.nvim",
+  version = "*",
+  config = function()
+    require("bufferline").setup{
+      options = {
+        indicator = { style = "underline" },
+        buffer_close_icon = "",
+        close_icon = "",
+      }
+    }
+  end,
+}
+
