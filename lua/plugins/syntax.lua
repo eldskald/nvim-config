10ce16b1d0688d7362c08637e27df3c6cb222144
rafael <rafaelbordoni00@gmail.com>
2023-05-07 21:31:01 -0300

    Add tree sitter

---
+++ lua/plugins/syntax.lua
@@ -0,0 +1,16 @@
+return {
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  configs = function ()
+    require('nvim-treesitter.configs').setup({
+      sync_install = false,
+      auto_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indend = { enable = true },
+    })
+  end,
+}
+
