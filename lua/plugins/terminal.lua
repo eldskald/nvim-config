f78083751fde507ca85408d8fbea7c8182d37234
rafael <rafaelbordoni00@gmail.com>
2023-05-17 01:16:05 -0300

    Update terminal controls

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -2,15 +2,15 @@ return {
   'voldikss/vim-floaterm',
   config = function()
     local map = vim.keymap
-    map.set('n', '<F12>', '<cmd> FloatermToggle <CR>')
-    map.set('t', '<F12>', '<C-\\><C-n>:FloatermToggle <CR>')
-    map.set('n', '<F8>', '<cmd> FloatermKill <CR>')
-    map.set('t', '<F8>', '<C-\\><C-n>:FloatermKill <CR>')
-    map.set('n', '<F9>', '<cmd> FloatermNew <CR>')
-    map.set('t', '<F9>', '<C-\\><C-n>:FloatermNew <CR>')
-    map.set('n', '<F10>', '<cmd> FloatermNext <CR>')
-    map.set('t', '<F10>', '<C-\\><C-n>:FloatermNext <CR>')
+    map.set('n', '<leader>t', '<cmd> FloatermToggle <CR>')
+    map.set('t', '<Esc>', '<C-\\><C-n>:FloatermToggle <CR>')
+    map.set('n', '<F9>', '<cmd> FloatermKill <CR>')
+    map.set('t', '<F9>', '<C-\\><C-n>:FloatermKill <CR>')
+    map.set('n', '<F10>', '<cmd> FloatermNew <CR>')
+    map.set('t', '<F10>', '<C-\\><C-n>:FloatermNew <CR>')
     map.set('n', '<F11>', '<cmd> FloatermPrev <CR>')
     map.set('t', '<F11>', '<C-\\><C-n>:FloatermPrev <CR>')
+    map.set('n', '<F12>', '<cmd> FloatermNext <CR>')
+    map.set('t', '<F12>', '<C-\\><C-n>:FloatermNext <CR>')
   end,
 }
