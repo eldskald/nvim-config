86062ad72d64e4d7c8a0432e2cd1fc511e744970
eldskald <rafaelbordoni00@gmail.com>
2023-12-10 16:31:59 -0300

    Add format on save

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -139,6 +139,7 @@ return {
         -- Linting
         local null_ls = require('null-ls')
         local b = null_ls.builtins
+        local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
 
         null_ls.setup({
             debug = true,
@@ -151,6 +152,22 @@ return {
                 b.formatting.stylua,
                 b.formatting.clang_format,
             },
+            -- Formatting on save
+            on_attach = function(client, bufnr)
+                if client.supports_method('textDocument/formatting') then
+                    vim.api.nvim_clear_autocmds({
+                        group = augroup,
+                        buffer = bufnr,
+                    })
+                    vim.api.nvim_create_autocmd('BufWritePre', {
+                        group = augroup,
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format({ async = false })
+                        end,
+                    })
+                end
+            end,
         })
 
         -- Diagnostics
